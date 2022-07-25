93994e8c17e9a6c4c9e6a4cb78fcb9af2391f28a
Andriy Polyakov <paspolyakov@gmail.com>
2022-07-25 22:45:40 +0300

    reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = ''; 
+    if (n<0) n *= -1;
+    while(n>0){ 
+     result = result + (n%10); 
+     n = parseInt(n/10); 
+    } 
+    return result; 
+  } 
+
